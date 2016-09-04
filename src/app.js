cf776592ba2c88cfb36031988aa4d62e9f3fa0e9
Yelyzaveta Buts <lizavetaageenko@gmail.com>
2016-09-04 23:58:28 +0300

    Add html file and some rendering logic

--- src/app.js
+++ src/app.js
@@ -1,2 +1,14 @@
 import simpsons from './one-more.js';
-console.log(simpsons);
+
+const body = document.getElementsByTagName('body')[0];
+
+const list = document.createElement('ul');
+
+body.appendChild(list);
+
+simpsons.forEach((heroName) => {
+    const heroListItem = document.createElement('li');
+    list.appendChild(heroListItem);
+
+    heroListItem.innerHTML = heroName;
+});
