ea69dbb84393310062643f1d64bdea0e9b37d654
Yelyzaveta Buts <lizavetaageenko@gmail.com>
2016-09-16 01:09:22 +0300

    Add session to express app

--- src/server.js
+++ src/server.js
@@ -1,9 +1,35 @@
 const express = require('express');
+const session = require('express-session');
 const app = express();
 const port = 3332;
 
+app.use(session({
+    secret: `don't look, this is a secret`,
+    resave: false,
+    saveUninitialized: true,
+    cookie: {
+        httpOnly: true,
+        secure: false
+    }
+}));
+
+function createNewGame(userSessionId) {
+    return {
+        id: 1,
+        status: 'new',
+        host: userSessionId,
+        players: [userSessionId],
+        location: null,
+        spy: null,
+        resolution: null
+    };
+}
+
 app.get('/', function (req, res) {
-    res.send('Hello World!');
+    const session = req.session;
+    console.log('New request:');
+    console.log(session.id);
+    res.send(createNewGame(session.id));
 });
 
 app.listen(port, function () {
